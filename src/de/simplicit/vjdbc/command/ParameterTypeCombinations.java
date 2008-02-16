e4a9ad2c2a808e8755beb5d82417ed631fad80b2
urmeli <urmeli@b62fc21d-d80f-0410-9b68-de9dbe04f5e5>
2008-02-16 23:03:21 +0000

    Added Properties-Combination
    git-svn-id: http://svn.code.sf.net/p/vjdbc/code@52 b62fc21d-d80f-0410-9b68-de9dbe04f5e5

--- src/de/simplicit/vjdbc/command/ParameterTypeCombinations.java
+++ src/de/simplicit/vjdbc/command/ParameterTypeCombinations.java
@@ -11,6 +11,7 @@ import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * This class holds an array of class arrays which can be efficiently used for Reflection
@@ -23,45 +24,46 @@ public class ParameterTypeCombinations {
         new Class[]{Integer.TYPE}, // 2
         new Class[]{String.class}, // 3
         new Class[]{Map.class}, // 4
+        new Class[]{Properties.class}, // 5
         // Pairs
-        new Class[]{Integer.TYPE, Integer.TYPE}, // 5
-        new Class[]{Integer.TYPE, Calendar.class}, // 6
-        new Class[]{String.class, Boolean.TYPE}, // 7
-        new Class[]{String.class, Byte.TYPE}, // 8
-        new Class[]{String.class, Short.TYPE}, // 9
-        new Class[]{String.class, Integer.TYPE}, // 10
-        new Class[]{String.class, Long.TYPE}, // 11
-        new Class[]{String.class, Float.TYPE}, // 12
-        new Class[]{String.class, Double.TYPE}, // 13
-        new Class[]{String.class, String.class}, // 14
-        new Class[]{String.class, Date.class}, // 15
-        new Class[]{String.class, Time.class}, // 16
-        new Class[]{String.class, Timestamp.class}, // 17
-        new Class[]{String.class, Calendar.class}, // 18
-        new Class[]{String.class, URL.class}, // 19
-        new Class[]{String.class, BigDecimal.class}, // 20
-        new Class[]{String.class, byte[].class}, // 21
-        new Class[]{String.class, int[].class}, // 22
-        new Class[]{String.class, String[].class}, // 23
+        new Class[]{Integer.TYPE, Integer.TYPE}, // 6
+        new Class[]{Integer.TYPE, Calendar.class}, // 7
+        new Class[]{String.class, Boolean.TYPE}, // 8
+        new Class[]{String.class, Byte.TYPE}, // 9
+        new Class[]{String.class, Short.TYPE}, // 10
+        new Class[]{String.class, Integer.TYPE}, // 11
+        new Class[]{String.class, Long.TYPE}, // 12
+        new Class[]{String.class, Float.TYPE}, // 13
+        new Class[]{String.class, Double.TYPE}, // 14
+        new Class[]{String.class, String.class}, // 15
+        new Class[]{String.class, Date.class}, // 16
+        new Class[]{String.class, Time.class}, // 17
+        new Class[]{String.class, Timestamp.class}, // 18
+        new Class[]{String.class, Calendar.class}, // 19
+        new Class[]{String.class, URL.class}, // 20
+        new Class[]{String.class, BigDecimal.class}, // 21
+        new Class[]{String.class, byte[].class}, // 22
+        new Class[]{String.class, int[].class}, // 23
+        new Class[]{String.class, String[].class}, // 24
         // Triples
-        new Class[]{Integer.TYPE, Integer.TYPE, Integer.TYPE}, // 24
-        new Class[]{String.class, Integer.TYPE, Integer.TYPE}, // 25
-        new Class[]{Integer.TYPE, Integer.TYPE, String.class}, // 26
-        new Class[]{String.class, Integer.TYPE, String.class}, // 27
-        new Class[]{String.class, String.class, String.class}, // 28
-        new Class[]{String.class, Date.class, Calendar.class}, // 29
-        new Class[]{String.class, Time.class, Calendar.class}, // 30
-        new Class[]{String.class, Timestamp.class, Calendar.class}, // 31
+        new Class[]{Integer.TYPE, Integer.TYPE, Integer.TYPE}, // 25
+        new Class[]{String.class, Integer.TYPE, Integer.TYPE}, // 26
+        new Class[]{Integer.TYPE, Integer.TYPE, String.class}, // 27
+        new Class[]{String.class, Integer.TYPE, String.class}, // 28
+        new Class[]{String.class, String.class, String.class}, // 29
+        new Class[]{String.class, Date.class, Calendar.class}, // 30
+        new Class[]{String.class, Time.class, Calendar.class}, // 31
+        new Class[]{String.class, Timestamp.class, Calendar.class}, // 32
         // Quad
-        new Class[]{String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE}, // 32
-        new Class[]{String.class, String.class, String.class, String.class}, // 33
-        new Class[]{String.class, String.class, String.class, String[].class}, // 34
-        new Class[]{String.class, String.class, String.class, int[].class}, // 35
+        new Class[]{String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE}, // 33
+        new Class[]{String.class, String.class, String.class, String.class}, // 34
+        new Class[]{String.class, String.class, String.class, String[].class}, // 35
+        new Class[]{String.class, String.class, String.class, int[].class}, // 36
         // Five
-        new Class[]{String.class, String.class, String.class, Integer.TYPE, Boolean.TYPE}, // 36
-        new Class[]{String.class, String.class, String.class, Boolean.TYPE, Boolean.TYPE}, // 37
+        new Class[]{String.class, String.class, String.class, Integer.TYPE, Boolean.TYPE}, // 37
+        new Class[]{String.class, String.class, String.class, Boolean.TYPE, Boolean.TYPE}, // 38
         // Six
-        new Class[]{String.class, String.class, String.class, String.class, String.class, String.class}// 38
+        new Class[]{String.class, String.class, String.class, String.class, String.class, String.class}// 39
     };
 
     // Single
@@ -69,43 +71,44 @@ public class ParameterTypeCombinations {
     public static final int INT = 2;
     public static final int STR = 3;
     public static final int MAP = 4;
+    public static final int PRP = 5;
     // Pairs
-    public static final int INTINT = 5;
-    public static final int INTCAL = 6;
-    public static final int STRBOL = 7;
-    public static final int STRBYT = 8;
-    public static final int STRSHT = 9;
-    public static final int STRINT = 10;
-    public static final int STRLNG = 11;
-    public static final int STRFLT = 12;
-    public static final int STRDBL = 13;
-    public static final int STRSTR = 14;
-    public static final int STRDAT = 15;
-    public static final int STRTIM = 16;
-    public static final int STRTMS = 17;
-    public static final int STRCAL = 18;
-    public static final int STRURL = 19;
-    public static final int STRBID = 20;
-    public static final int STRBYTA = 21;
-    public static final int STRINTA = 22;
-    public static final int STRSTRA = 23;
-    // Triples
-    public static final int INTINTINT = 24;
-    public static final int STRINTINT = 25;
-    public static final int INTINTSTR = 26;
-    public static final int STRINTSTR = 27;
-    public static final int STRSTRSTR = 28;
-    public static final int STRDATCAL = 29;
-    public static final int STRTIMCAL = 30;
-    public static final int STRTMSCAL = 31;
+    public static final int INTINT = 6;
+    public static final int INTCAL = 7;
+    public static final int STRBOL = 8;
+    public static final int STRBYT = 9;
+    public static final int STRSHT = 10;
+    public static final int STRINT = 11;
+    public static final int STRLNG = 12;
+    public static final int STRFLT = 13;
+    public static final int STRDBL = 14;
+    public static final int STRSTR = 15;
+    public static final int STRDAT = 16;
+    public static final int STRTIM = 17;
+    public static final int STRTMS = 18;
+    public static final int STRCAL = 19;
+    public static final int STRURL = 20;
+    public static final int STRBID = 21;
+    public static final int STRBYTA = 22;
+    public static final int STRINTA = 23;
+    public static final int STRSTRA = 24;
+    // Triple
+    public static final int INTINTINT = 25;
+    public static final int STRINTINT = 26;
+    public static final int INTINTSTR = 27;
+    public static final int STRINTSTR = 28;
+    public static final int STRSTRSTR = 29;
+    public static final int STRDATCAL = 30;
+    public static final int STRTIMCAL = 31;
+    public static final int STRTMSCAL = 32;
     // Quad
-    public static final int STRINTINTINT = 32;
-    public static final int STRSTRSTRSTR = 33;
-    public static final int STRSTRSTRSTRA = 34;
-    public static final int STRSTRSTRINTA = 35;
+    public static final int STRINTINTINT = 33;
+    public static final int STRSTRSTRSTR = 34;
+    public static final int STRSTRSTRSTRA = 35;
+    public static final int STRSTRSTRINTA = 36;
     // Five
-    public static final int STRSTRSTRINTBOL = 36;
-    public static final int STRSTRSTRBOLBOL = 37;
+    public static final int STRSTRSTRINTBOL = 37;
+    public static final int STRSTRSTRBOLBOL = 38;
     // Six
-    public static final int STRSTRSTRSTRSTRSTR = 38;
+    public static final int STRSTRSTRSTRSTRSTR = 39;
 }
